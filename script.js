1956ec92444b692c1618aab1caa4b2a292f9a211
dlenh <dlenh425@gmail.com>
2022-04-19 13:17:13 -0700

    Created variables for the event listeners.

--- script.js
+++ script.js
@@ -69,12 +69,13 @@ fetch(`https://ghibliapi.herokuapp.com/films`)
         }
 
         totalMin = 0; 
+        total.innerText = totalMin;
 
         const minutes = {
             item1: () => {
                 clicks.c1 ? totalMin += arr[0] : totalMin -= arr[0];
                 if (!item1.style.border) {
-                    item1.style.border = "2px red solid";
+                    item1.style.border = "0.25rem red solid";
                 } else {
                     item1.style.border = "";
                 }
@@ -84,7 +85,7 @@ fetch(`https://ghibliapi.herokuapp.com/films`)
             item2: () => {
                 clicks.c2 ? totalMin += arr[1] : totalMin -= arr[1];
                 if (!item2.style.border) {
-                    item2.style.border = "2px red solid";
+                    item2.style.border = "0.25rem red solid";
                 } else {
                     item2.style.border = "";
                 }
@@ -94,7 +95,7 @@ fetch(`https://ghibliapi.herokuapp.com/films`)
             item3: () => {
                 clicks.c3 ? totalMin += arr[2] : totalMin -= arr[2];
                 if (!item3.style.border) {
-                    item3.style.border = "2px red solid";
+                    item3.style.border = "0.25rem red solid";
                 } else {
                     item3.style.border = "";
                 }
@@ -104,7 +105,7 @@ fetch(`https://ghibliapi.herokuapp.com/films`)
             item4: () => {
                 clicks.c4 ? totalMin += arr[3] : totalMin -= arr[3];
                 if (!item4.style.border) {
-                    item4.style.border = "2px red solid";
+                    item4.style.border = "0.25rem red solid";
                 } else {
                     item4.style.border = "";
                 }
@@ -114,7 +115,7 @@ fetch(`https://ghibliapi.herokuapp.com/films`)
             item5: () => {
                 clicks.c5 ? totalMin += arr[4] : totalMin -= arr[4];
                 if (!item5.style.border) {
-                    item5.style.border = "2px red solid";
+                    item5.style.border = "0.25rem red solid";
                 } else {
                     item5.style.border = "";
                 }
@@ -124,7 +125,7 @@ fetch(`https://ghibliapi.herokuapp.com/films`)
             item6: () => {
                 clicks.c6 ? totalMin += arr[5] : totalMin -= arr[5];
                 if (!item6.style.border) {
-                    item6.style.border = "2px red solid";
+                    item6.style.border = "0.25rem red solid";
                 } else {
                     item6.style.border = "";
                 }
@@ -134,7 +135,7 @@ fetch(`https://ghibliapi.herokuapp.com/films`)
             item7: () => {
                 clicks.c7 ? totalMin += arr[6] : totalMin -= arr[6];
                 if (!item7.style.border) {
-                    item7.style.border = "2px red solid";
+                    item7.style.border = "0.25rem red solid";
                 } else {
                     item7.style.border = "";
                 }
@@ -144,7 +145,7 @@ fetch(`https://ghibliapi.herokuapp.com/films`)
             item8: () => {
                 clicks.c8 ? totalMin += arr[7] : totalMin -= arr[7];
                 if (!item8.style.border) {
-                    item8.style.border = "2px red solid";
+                    item8.style.border = "0.25rem red solid";
                 } else {
                     item8.style.border = "";
                 }
@@ -154,7 +155,7 @@ fetch(`https://ghibliapi.herokuapp.com/films`)
             item9: () => {
                 clicks.c9 ? totalMin += arr[8] : totalMin -= arr[8];
                 if (!item9.style.border) {
-                    item9.style.border = "2px red solid";
+                    item9.style.border = "0.25rem red solid";
                 } else {
                     item9.style.border = "";
                 }
@@ -164,7 +165,7 @@ fetch(`https://ghibliapi.herokuapp.com/films`)
             item10: () => {
                 clicks.c10 ? totalMin += arr[9] : totalMin -= arr[9];
                 if (!item10.style.border) {
-                    item10.style.border = "2px red solid";
+                    item10.style.border = "0.25rem red solid";
                 } else {
                     item10.style.border = "";
                 }
@@ -174,7 +175,7 @@ fetch(`https://ghibliapi.herokuapp.com/films`)
             item11: () => {
                 clicks.c11 ? totalMin += arr[10] : totalMin -= arr[10];
                 if (!item11.style.border) {
-                    item11.style.border = "2px red solid";
+                    item11.style.border = "0.25rem red solid";
                 } else {
                     item11.style.border = "";
                 }
@@ -184,7 +185,7 @@ fetch(`https://ghibliapi.herokuapp.com/films`)
             item12: () => {
                 clicks.c12 ? totalMin += arr[11] : totalMin -= arr[11];
                 if (!item12.style.border) {
-                    item12.style.border = "2px red solid";
+                    item12.style.border = "0.25rem red solid";
                 } else {
                     item12.style.border = "";
                 }
@@ -194,7 +195,7 @@ fetch(`https://ghibliapi.herokuapp.com/films`)
             item13: () => {
                 clicks.c13 ? totalMin += arr[12] : totalMin -= arr[12];
                 if (!item13.style.border) {
-                    item13.style.border = "2px red solid";
+                    item13.style.border = "0.25rem red solid";
                 } else {
                     item13.style.border = "";
                 }
@@ -204,7 +205,7 @@ fetch(`https://ghibliapi.herokuapp.com/films`)
             item14: () => {
                 clicks.c14 ? totalMin += arr[13] : totalMin -= arr[13];
                 if (!item14.style.border) {
-                    item14.style.border = "2px red solid";
+                    item14.style.border = "0.25rem red solid";
                 } else {
                     item14.style.border = "";
                 }
@@ -214,7 +215,7 @@ fetch(`https://ghibliapi.herokuapp.com/films`)
             item15: () => {
                 clicks.c15 ? totalMin += arr[14] : totalMin -= arr[14];
                 if (!item15.style.border) {
-                    item15.style.border = "2px red solid";
+                    item15.style.border = "0.25rem red solid";
                 } else {
                     item15.style.border = "";
                 }
@@ -224,7 +225,7 @@ fetch(`https://ghibliapi.herokuapp.com/films`)
             item16: () => {
                 clicks.c16 ? totalMin += arr[15] : totalMin -= arr[15];
                 if (!item16.style.border) {
-                    item16.style.border = "2px red solid";
+                    item16.style.border = "0.25rem red solid";
                 } else {
                     item16.style.border = "";
                 }
@@ -234,7 +235,7 @@ fetch(`https://ghibliapi.herokuapp.com/films`)
             item17: () => {
                 clicks.c17 ? totalMin += arr[16] : totalMin -= arr[16];
                 if (!item17.style.border) {
-                    item17.style.border = "2px red solid";
+                    item17.style.border = "0.25rem red solid";
                 } else {
                     item17.style.border = "";
                 }
@@ -244,7 +245,7 @@ fetch(`https://ghibliapi.herokuapp.com/films`)
             item18: () => {
                 clicks.c18 ? totalMin += arr[17] : totalMin -= arr[17];
                 if (!item18.style.border) {
-                    item18.style.border = "2px red solid";
+                    item18.style.border = "0.25rem red solid";
                 } else {
                     item18.style.border = "";
                 }
@@ -254,7 +255,7 @@ fetch(`https://ghibliapi.herokuapp.com/films`)
             item19: () => {
                 clicks.c19 ? totalMin += arr[18] : totalMin -= arr[18];
                 if (!item19.style.border) {
-                    item19.style.border = "2px red solid";
+                    item19.style.border = "0.25rem red solid";
                 } else {
                     item19.style.border = "";
                 }
@@ -264,7 +265,7 @@ fetch(`https://ghibliapi.herokuapp.com/films`)
             item20: () => {
                 clicks.c20 ? totalMin += arr[19] : totalMin -= arr[19];
                 if (!item20.style.border) {
-                    item20.style.border = "2px red solid";
+                    item20.style.border = "0.25rem red solid";
                 } else {
                     item20.style.border = "";
                 }
@@ -274,7 +275,7 @@ fetch(`https://ghibliapi.herokuapp.com/films`)
             item21: () => {
                 clicks.c21 ? totalMin += arr[20] : totalMin -= arr[20];
                 if (!item21.style.border) {
-                    item21.style.border = "2px red solid";
+                    item21.style.border = "0.25rem red solid";
                 } else {
                     item21.style.border = "";
                 }
@@ -284,7 +285,7 @@ fetch(`https://ghibliapi.herokuapp.com/films`)
             item22: () => {
                 clicks.c22 ? totalMin += arr[21] : totalMin -= arr[21];
                 if (!item22.style.border) {
-                    item22.style.border = "2px red solid";
+                    item22.style.border = "0.25rem red solid";
                 } else {
                     item22.style.border = "";
                 }
@@ -293,28 +294,34 @@ fetch(`https://ghibliapi.herokuapp.com/films`)
             }
         }
 
-        document.querySelector("#item1").addEventListener("click", minutes.item1)
-        document.querySelector("#item2").addEventListener("click", minutes.item2)
-        document.querySelector("#item3").addEventListener("click", minutes.item3)
-        document.querySelector("#item4").addEventListener("click", minutes.item4)
-        document.querySelector("#item5").addEventListener("click", minutes.item5)
-        document.querySelector("#item6").addEventListener("click", minutes.item6)
-        document.querySelector("#item7").addEventListener("click", minutes.item7)
-        document.querySelector("#item8").addEventListener("click", minutes.item8)
-        document.querySelector("#item9").addEventListener("click", minutes.item9)
-        document.querySelector("#item10").addEventListener("click", minutes.item10)
-        document.querySelector("#item11").addEventListener("click", minutes.item11)
-        document.querySelector("#item12").addEventListener("click", minutes.item12)
-        document.querySelector("#item13").addEventListener("click", minutes.item13)
-        document.querySelector("#item14").addEventListener("click", minutes.item14)
-        document.querySelector("#item15").addEventListener("click", minutes.item15)
-        document.querySelector("#item16").addEventListener("click", minutes.item16)
-        document.querySelector("#item17").addEventListener("click", minutes.item17)
-        document.querySelector("#item18").addEventListener("click", minutes.item18)
-        document.querySelector("#item19").addEventListener("click", minutes.item19)
-        document.querySelector("#item20").addEventListener("click", minutes.item20)
-        document.querySelector("#item21").addEventListener("click", minutes.item21)
-        document.querySelector("#item22").addEventListener("click", minutes.item22)
+        if (totalMin != 0) {
+            const button = document.createElement("button")
+            button.textContent = "Convert Time"
+            document.querySelector("div").appendChild(button)
+        }
+
+        item1.addEventListener("click", minutes.item1)
+        item2.addEventListener("click", minutes.item2)
+        item3.addEventListener("click", minutes.item3)
+        item4.addEventListener("click", minutes.item4)
+        item5.addEventListener("click", minutes.item5)
+        item6.addEventListener("click", minutes.item6)
+        item7.addEventListener("click", minutes.item7)
+        item8.addEventListener("click", minutes.item8)
+        item9.addEventListener("click", minutes.item9)
+        item10.addEventListener("click", minutes.item10)
+        item11.addEventListener("click", minutes.item11)
+        item12.addEventListener("click", minutes.item12)
+        item13.addEventListener("click", minutes.item13)
+        item14.addEventListener("click", minutes.item14)
+        item15.addEventListener("click", minutes.item15)
+        item16.addEventListener("click", minutes.item16)
+        item17.addEventListener("click", minutes.item17)
+        item18.addEventListener("click", minutes.item18)
+        item19.addEventListener("click", minutes.item19)
+        item20.addEventListener("click", minutes.item20)
+        item21.addEventListener("click", minutes.item21)
+        item22.addEventListener("click", minutes.item22)
 
     })
     .catch(err => {
